3627b22ef4576b6ed10278bb6785808b2e665382
Дмитрий <success-gala@rambler.ru>
2024-11-15 15:49:10 +0300

    Теперь каждое изображение в отдельном окне. Добавлены спинбоксы для установления размеров окна.

--- 9.2_03 Dogs.py
+++ 9.2_03 Dogs.py
@@ -24,10 +24,16 @@ def show_image():
             response.raise_for_status()
             img_date=BytesIO(response.content)
             img=Image.open(img_date)
-            img.thumbnail((300, 300))
+            img_size=(int(width_spinbox.get()), int(height_spinbox.get()))
+            img.thumbnail(img_size)
             img=ImageTk.PhotoImage(img)
-            label.config(image=img)
+
+            new_window=Toplevel(window)
+            new_window.title("Случайное изображение собакена!")
+            label=ttk.Label(new_window, image=img)
+            #label.config(image=img)
             label.image=img
+            label.pack(padx=10, pady=10)
         except Exception as e:
             mb.showerror("Ошибка", f"Возникла ошибка при загрузке изображений {e}")
 
@@ -47,5 +53,15 @@ button.pack(padx=10, pady=10)
 
 progress=ttk.Progressbar(mode='determinate', length=300)
 progress.pack(padx=10, pady=10)
+
+width_label=ttk.Label(text="Ширина:")
+width_label.pack(side='left', padx=(10, 0))
+width_spinbox=ttk.Spinbox(from_=200, to=500, increment=50, width=5)
+width_spinbox.pack(side='left', padx=(0, 10))
+
+height_label=ttk.Label(text="Высота:")
+height_label.pack(side='left', padx=(10, 0))
+height_spinbox=ttk.Spinbox(from_=200, to=500, increment=50, width=5)
+height_spinbox.pack(side='left', padx=(0, 10))
 
 window.mainloop()
